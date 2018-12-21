4a62a105849605abfb90c1e1cd1414e6db0d95c3
Nick Espig <nickespig@gmail.com>
2018-12-21 09:56:32 +0100

    Create iterator for pull requests

--- fun/parse_data.py
+++ fun/parse_data.py
@@ -9,6 +9,12 @@ with open(GITHUB_DATA_PATH) as json_file:
     big_ass_data = json.load(json_file)
 
 
+def pull_iterator():
+    for repo in big_ass_data.values():
+        for pull in repo['pull_requests']:
+            yield pull
+
+
 def dates_pull_request_was_not_merged(pull_request):
     merged = datetime.fromisoformat(pull_request['merged_at'][:-1])
     created = datetime.fromisoformat(pull_request['created_at'][:-1])
@@ -24,26 +30,24 @@ def dates_pull_request_was_not_merged(pull_request):
 def merge_duration_over_time():
     over_time = {}
 
-    for repo in big_ass_data.values():
-        for pull in repo['pull_requests']:
-            if not pull['merged_at']:
-                continue
+    for pull in pull_iterator():
+        if not pull['merged_at']:
+            continue
 
-            for day in dates_pull_request_was_not_merged(pull):
-                over_time.setdefault(day, [])
-                over_time[day].append(pull)
+        for day in dates_pull_request_was_not_merged(pull):
+            over_time.setdefault(day, [])
+            over_time[day].append(pull)
 
     return sorted(over_time.items(), key=lambda item: item[0])
 
 
 def open_pr_per_person():
     people = {}
-    for repo in big_ass_data.values():
-        for pull in repo['pull_requests']:
-            if not pull['state'] == 'open':
-                continue
-            user = pull['user']['login']
-            people.setdefault(user, [])
-            people[user].append(pull)
+    for pull in pull_iterator():
+        if not pull['state'] == 'open':
+            continue
+        user = pull['user']['login']
+        people.setdefault(user, [])
+        people[user].append(pull)
 
     return sorted(people.items(), key=lambda item: item[0])
